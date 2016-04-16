f5534b2cdf559653697ee82c480cd0bc2c2e94fa
unknown <>
2016-04-17 02:02:00 +0800

    fix

--- WebRoot/checkover.jsp
+++ WebRoot/checkover.jsp
@@ -1,4 +1,5 @@
-<%@ page language="java" import="java.util.*" import="com.absence.hibernate.beans.*" pageEncoding="utf-8"%>
+<%@ page language="java" import="java.util.*"
+	import="com.absence.hibernate.beans.*" pageEncoding="utf-8"%>
 <%
 String path = request.getContextPath();
 String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
@@ -6,50 +7,58 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
-  <head>
-    <base href="<%=basePath%>">
-    
-    <title>查看已审批假条</title>
-    
-	<meta http-equiv="pragma" content="no-cache">
-	<meta http-equiv="cache-control" content="no-cache">
-	<meta http-equiv="expires" content="0">    
-	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
-	<meta http-equiv="description" content="This is my page">
-	<!--
+<head>
+<base href="<%=basePath%>">
+
+<title>查看已审批假条</title>
+
+<meta http-equiv="pragma" content="no-cache">
+<meta http-equiv="cache-control" content="no-cache">
+<meta http-equiv="expires" content="0">
+<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
+<meta http-equiv="description" content="This is my page">
+<!--
 	<link rel="stylesheet" type="text/css" href="styles.css">
 	-->
 
-  </head>
-  
-  <body  topmargin="60">
-   <% 
+</head>
+
+<body topmargin="60">
+	<% 
       request.setCharacterEncoding("utf-8");   
       ArrayList<Examine> list=(ArrayList<Examine>)request.getAttribute("list"); 
        
     %>
-    <table align="center" border="1" width="700" cellSpacing="0" borderColorLight="#c0c0c0" 
-      borderColorDark="#ffffff" width="600" bgColor="#f0f5fd"  cellPadding="4">
-       <tr height="50">
-          <td colspan="5" align="center"><FONT style="FONT-SIZE: 15pt" color=#ff0000>查看学生假条的审核情况</FONT></td>
-       </tr>
-       <tr>
-          <td align="center">假条编号</td><td align="center">审核时间</td><td align="center">审核</td><td align="center">审核人</td><td>&nbsp;</td>
-       </tr>
-        <%
+	<table align="center" border="1" width="700" cellSpacing="0"
+		borderColorLight="#c0c0c0" borderColorDark="#ffffff" width="600"
+		bgColor="#f0f5fd" cellPadding="4">
+		<tr height="50">
+			<td colspan="5" align="center"><FONT style="FONT-SIZE: 15pt"
+				color=#ff0000>查看学生假条的审核情况</FONT></td>
+		</tr>
+		<tr>
+			<td align="center">假条编号</td>
+			<td align="center">审核时间</td>
+			<td align="center">审核</td>
+			<td align="center">审核人</td>
+			<td>&nbsp;</td>
+		</tr>
+		<%
         for(int i=0;i<list.size();i++){
               Examine cation = (Examine)list.get(i);
-         %>  
-       <tr>
-          <td align="center"><a href="examin.do?account=<%=(String)session.getAttribute("account") %>&id=<%=cation.getId() %>"><%=cation.getId() %></a></td>
-          <td align="center"><%=cation.getExamtime()%></td>
-          <td align="center"><%=cation.getPermit()%></td>
-          <td align="center"><%=cation.getAdminid()%></td>
-          <td align="center"><a href="Tdelect.do?account=<%=(String)session.getAttribute("account") %>&id=<%=cation.getId() %>">删除</a></td>
-       </tr>
-       <%
+         %>
+		<tr>
+			<td align="center"><a
+				href="examin.do?account=<%=(String)session.getAttribute("account") %>&id=<%=cation.getId() %>"><%=cation.getId() %></a></td>
+			<td align="center"><%=cation.getExamtime()%></td>
+			<td align="center"><%=cation.getPermit()%></td>
+			<td align="center"><%=cation.getAdminid()%></td>
+			<td align="center"><a
+				href="Tdelect.do?account=<%=(String)session.getAttribute("account") %>&id=<%=cation.getId() %>">删除</a></td>
+		</tr>
+		<%
           }
         %>
-    </table>
-  </body>
+	</table>
+</body>
 </html>
--- src/com/absence/struts/dao/QingjiaDAO.java
+++ src/com/absence/struts/dao/QingjiaDAO.java
@@ -13,175 +13,182 @@ import com.absence.hibernate.beans.Student;
 import com.absence.hibernate.util.HibernateSessionFactory;
 
 public class QingjiaDAO {
-		  public ArrayList<Application> search(String id)throws HibernateException{
-		    Session session=null;
-		    Transaction tx=null;
-		    ArrayList<Application> list=null;
-		    try{
-		    	session=HibernateSessionFactory.getSession();
-		    	tx=session.beginTransaction();  
-		    	Query query=null;
-		        query=session.createQuery("from Application where xuehao=?");
-		    	query.setString(0, id.trim());
-		    	System.out.println(query);
-		    	list=(ArrayList<Application>)query.list();
-		    	query=null;
-		    	tx.commit();
-		    	tx=null;
-		    }catch(HibernateException e){
-		    	throw e;
-		    }finally{
-		    	if(tx!=null){
-		    		tx.rollback();
-		    	}
-		    	HibernateSessionFactory.closeSession();
-		    }
-		    return list;
-		  }
-		  
-		  public Application Update(int id)throws HibernateException{
-			    Session session=null;
-			    Transaction tx=null;
-			    Application user=null;
-			    try{
-			    	session=HibernateSessionFactory.getSession();
-			    	tx=session.beginTransaction();  
-			    	Query query=null;
-			        query=session.createQuery("from Application where id=?");
-			    	query.setInteger(0, id);
-			    	System.out.println(query);
-			    	System.out.println(query.uniqueResult());
-			    	user=(Application)query.uniqueResult();
-			    	query=null;
-			    	tx.commit();
-			    	tx=null;
-			    }catch(HibernateException e){
-			    	throw e;
-			    }finally{
-			    	if(tx!=null){
-			    		tx.rollback();
-			    	}
-			    	HibernateSessionFactory.closeSession();
-			    }
-			    return user;
-			  }
-		  
-		  public void Delect(int id)throws HibernateException{
-			  Session session=null;
-			  Transaction tx=null;
-			  try{
-				  session=HibernateSessionFactory.getSession();
-				  tx=session.beginTransaction();
-				  Query query=null;
-				  query=session.createQuery("from Examine where id=?");
-			      query.setInteger(0, id);
-			      System.out.println(query);
-			      System.out.println(query.uniqueResult());
-			      Examine exam=(Examine)query.uniqueResult();
-				  session.delete(exam);
-				  session.flush();
-			      query=session.createQuery("from Application where id=?");
-			      query.setInteger(0, id);
-			      System.out.println(query);
-			      System.out.println(query.uniqueResult());
-			      Application user=(Application)query.uniqueResult();
-				  session.delete(user);
-				  session.flush();
-				  tx.commit();
-				  tx=null;
-			  }catch(HibernateException e){
-				  throw e;
-				  
-			  }finally{
-				  if(tx!=null){tx.rollback();
-			  }
-			  HibernateSessionFactory.closeSession();
-		  }
-		  }
-		  //保存更新后的数据
-		  public boolean Save(int id,String teachclass,String begintime,String overtime,String reason)throws HibernateException{
-			  Session session =null;
-			  Transaction tx=null;
-			  boolean b=true;
-			  try{
-				  session=HibernateSessionFactory.getSession();
-				  tx=session.beginTransaction();
-				  Query query=null;
-			      query=session.createQuery("from Application where id=?");
-			      query.setInteger(0, id);
-			      System.out.println(query);
-			      System.out.println(query.uniqueResult());
-			      Application user=(Application)query.uniqueResult();
-				  user.setTeachclass(teachclass);
-				  user.setBegintime(begintime);
-				  user.setOvertime(overtime);
-				  user.setReason(reason);
-				  session.save(user);
-				  tx.commit();
-				  tx=null;
-			  }catch(HibernateException e){
-				  b=false;
-				  throw e;
-			      }finally{if(tx!=null){tx.rollback();}
-			      HibernateSessionFactory.closeSession();
-			      }
-			  return b;
-			  
-			  }
-		  
-		  public Student Select(String xuehao)throws HibernateException{
-			    Session session=null;
-			    Transaction tx=null;
-			    Student user=null;
-			    try{
-			    	session=HibernateSessionFactory.getSession();
-			    	tx=session.beginTransaction();  
-			    	System.out.println(xuehao+"-----"+xuehao.trim()+"---");
-			    	Query query=null;
-			        query=session.createQuery("from Student where id=?");
-			    	query.setString(0, xuehao.trim());
-			    	System.out.println(query);
-			    	System.out.println(query.uniqueResult());
-			    	user=(Student)query.uniqueResult();
-			    	query=null;
-			    	tx.commit();
-			    	tx=null;
-			    }catch(HibernateException e){
-			    	throw e;
-			    }finally{
-			    	if(tx!=null){
-			    		tx.rollback();
-			    	}
-			    	HibernateSessionFactory.closeSession();
-			    }
-			    return user;
-			  }
-		  
-		  public Examine Select_P(int id)throws HibernateException{
-			    Session session=null;
-			    Transaction tx=null;
-			    Examine user=null;
-			    try{
-			    	session=HibernateSessionFactory.getSession();
-			    	tx=session.beginTransaction();  
-			    	System.out.println(id+"--------");
-			    	Query query=null;
-			        query=session.createQuery("from Examine where id=?");
-			    	query.setInteger(0, id);
-			    	System.out.println(query);
-			    	System.out.println(query.uniqueResult());
-			    	user=(Examine)query.uniqueResult();
-			    	query=null;
-			    	tx.commit();
-			    	tx=null;
-			    }catch(HibernateException e){
-			    	throw e;
-			    }finally{
-			    	if(tx!=null){
-			    		tx.rollback();
-			    	}
-			    	HibernateSessionFactory.closeSession();
-			    }
-			    return user;
-			  }
+	public ArrayList<Application> search(String id) throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		ArrayList<Application> list = null;
+		try {
+			session = HibernateSessionFactory.getSession();
+			tx = session.beginTransaction();
+			Query query = null;
+			query = session.createQuery("from Application where xuehao=?");
+			query.setString(0, id.trim());
+			System.out.println(query);
+			list = (ArrayList<Application>) query.list();
+			System.out.println(list);
+			query = null;
+			tx.commit();
+			tx = null;
+		} catch (HibernateException e) {
+			throw e;
+		} finally {
+			if (tx != null) {
+				tx.rollback();
+			}
+			HibernateSessionFactory.closeSession();
+		}
+		return list;
+	}
+
+	public Application Update(int id) throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		Application user = null;
+		try {
+			session = HibernateSessionFactory.getSession();
+			tx = session.beginTransaction();
+			Query query = null;
+			query = session.createQuery("from Application where id=?");
+			query.setInteger(0, id);
+			System.out.println(query);
+			System.out.println(query.uniqueResult());
+			user = (Application) query.uniqueResult();
+			query = null;
+			tx.commit();
+			tx = null;
+		} catch (HibernateException e) {
+			throw e;
+		} finally {
+			if (tx != null) {
+				tx.rollback();
+			}
+			HibernateSessionFactory.closeSession();
+		}
+		return user;
+	}
+
+	public void Delect(int id) throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		try {
+			session = HibernateSessionFactory.getSession();
+			tx = session.beginTransaction();
+			Query query = null;
+			query = session.createQuery("from Examine where id=?");
+			query.setInteger(0, id);
+			System.out.println(query);
+			System.out.println(query.uniqueResult());
+			Examine exam = (Examine) query.uniqueResult();
+			session.delete(exam);
+			session.flush();
+			query = session.createQuery("from Application where id=?");
+			query.setInteger(0, id);
+			System.out.println(query);
+			System.out.println(query.uniqueResult());
+			Application user = (Application) query.uniqueResult();
+			session.delete(user);
+			session.flush();
+			tx.commit();
+			tx = null;
+		} catch (HibernateException e) {
+			throw e;
+
+		} finally {
+			if (tx != null) {
+				tx.rollback();
+			}
+			HibernateSessionFactory.closeSession();
+		}
+	}
+
+	// 保存更新后的数据
+	public boolean Save(int id, String teachclass, String begintime, String overtime, String reason)
+			throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		boolean b = true;
+		try {
+			session = HibernateSessionFactory.getSession();
+			tx = session.beginTransaction();
+			Query query = null;
+			query = session.createQuery("from Application where id=?");
+			query.setInteger(0, id);
+			System.out.println(query);
+			System.out.println(query.uniqueResult());
+			Application user = (Application) query.uniqueResult();
+			user.setTeachclass(teachclass);
+			user.setBegintime(begintime);
+			user.setOvertime(overtime);
+			user.setReason(reason);
+			session.save(user);
+			tx.commit();
+			tx = null;
+		} catch (HibernateException e) {
+			b = false;
+			throw e;
+		} finally {
+			if (tx != null) {
+				tx.rollback();
+			}
+			HibernateSessionFactory.closeSession();
+		}
+		return b;
+
+	}
+
+	public Student Select(String xuehao) throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		Student user = null;
+		try {
+			session = HibernateSessionFactory.getSession();
+			tx = session.beginTransaction();
+			System.out.println(xuehao + "-----" + xuehao.trim() + "---");
+			Query query = null;
+			query = session.createQuery("from Student where id=?");
+			query.setString(0, xuehao.trim());
+			System.out.println(query);
+			System.out.println(query.uniqueResult());
+			user = (Student) query.uniqueResult();
+			query = null;
+			tx.commit();
+			tx = null;
+		} catch (HibernateException e) {
+			throw e;
+		} finally {
+			if (tx != null) {
+				tx.rollback();
+			}
+			HibernateSessionFactory.closeSession();
+		}
+		return user;
+	}
+
+	public Examine Select_P(int id) throws HibernateException {
+		Session session = null;
+		Transaction tx = null;
+		Examine user = null;
+		try {
+			session = HibernateSessionFactory.getSession();
+			tx = session.beginTransaction();
+			System.out.println(id + "--------");
+			Query query = null;
+			query = session.createQuery("from Examine where id=?");
+			query.setInteger(0, id);
+			System.out.println(query);
+			System.out.println(query.uniqueResult());
+			user = (Examine) query.uniqueResult();
+			query = null;
+			tx.commit();
+			tx = null;
+		} catch (HibernateException e) {
+			throw e;
+		} finally {
+			if (tx != null) {
+				tx.rollback();
+			}
+			HibernateSessionFactory.closeSession();
+		}
+		return user;
+	}
 }
